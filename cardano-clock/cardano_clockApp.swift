b8d87a22dc452eefffbf5483404cb6ca787ef4f1
Nolan Kramer <kramernn@gmail.com>
2022-03-14 21:38:32 -0700

    Add all files

---
+++ cardano-clock/cardano_clockApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardano_clockApp.swift
+//  cardano-clock
+//
+//  Created by Nolan Kramer on 3/14/22.
+//
+
+import SwiftUI
+
+@main
+struct cardano_clockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
